1e8e84b00cc2ae0b15811f50e248c17a2d9df338
Sneha Das <dassa@iis.fraunhofer.de>
2015-10-19 23:30:06 +0200

    restructuring

--- stmcb/stmcb_test_script.m
+++
@@ -1,5 +0,0 @@
-  [b,a] = butter(10,0.2);              % Butterworth filter design
-  h = filter(b,a,[1 zeros(1,100)]);   % Filter data using above filter
-  %freqz(b,a,128)                      % Frequency response
-  [bb,aa] = stmcb(h, [1 zeros(1,100)], 8, 8, 10);
-  figure; freqz(bb,aa,128)
